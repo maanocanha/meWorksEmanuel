5efbf941653e9fbd97a060b3ad66f3712b4858ce
Marco Antonio Rosa da Silva <marco.silva@unochapeco.edu.br>
2019-07-07 22:09:55 -0300

    Rotas inseridas
    Rotas foram definidas e agora as telas criadas podem ser acessadas pela tela inicial sem a necessidade de comentar e descomentar código

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { LoginComponent } from './login/login.component';
+import { PerfilComponent } from './perfil/perfil.component';
+import { PrestadorComponent } from './prestador/prestador.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'cadastro', component: CadastroComponent },
+  { path: 'perfil', component: PerfilComponent },
+  { path: 'prestador', component: PrestadorComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
