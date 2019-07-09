224e2ee43d8960af982d80d0c2b03e1542910ee3
Marco Antonio Rosa da Silva <marco.silva@unochapeco.edu.br>
2019-07-08 23:45:47 -0300

    Mais alterações
    Estou cansado :(

--- src/app/not-found/not-found.component.ts
+++ src/app/not-found/not-found.component.ts
@@ -2,10 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-not-found',
-  template: `
-    <h3>Erro 404. Esta página não existe!</h3>
-  `,
-  styles: []
+  templateUrl: './not-found.component.html',
+  styleUrls: ['./not-found.component.css']
 })
 export class NotFoundComponent implements OnInit {
 
